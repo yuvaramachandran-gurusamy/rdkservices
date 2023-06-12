8c5047e03a22ca8f3c07476341336ad79b7dacc1
rajnishdey <rajnishdey@users.noreply.github.com>
2023-06-12 19:22:04 -0400

    Update MiracastPlayer.h

--- MiracastService/MiracastPlayer.h
+++ MiracastService/MiracastPlayer.h
@@ -51,6 +51,7 @@ private:
     bool m_bReady;
     int m_buffering_level;
     double m_currentPosition;
+    GstElement *m_video_sink{nullptr};
     pthread_t m_playback_thread;
 
     static MiracastPlayer *mMiracastPlayer;
@@ -68,6 +69,9 @@ private:
     static void *playbackThread(void *ctx);
     GMainLoop *m_main_loop{nullptr};
     GMainContext *m_main_loop_context{nullptr};
+    
+    pthread_t m_player_statistics_tid;
+    static void *monitor_player_statistics_thread(void *ctx);
 };
 
 #endif /* MiracastPlayer_hpp */
