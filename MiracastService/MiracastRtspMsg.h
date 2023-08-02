740fcda04d040d75982fa764945345692ce24e05
yuvaramachandran_gurusamy <yuvaramachandran_gurusamy@comcast.com>
2023-08-02 11:18:33 +0000

    Modified the RTSP receive buffer timedout as 6seconds.

--- MiracastService/MiracastRtspMsg.h
+++ MiracastService/MiracastRtspMsg.h
@@ -28,6 +28,7 @@
 
 #define MAX_EPOLL_EVENTS 64
 #define SOCKET_DFLT_WAIT_TIMEOUT (30 * ONE_SECOND_IN_MILLISEC)
+#define RTSP_RECV_WAIT_TIMEOUT (6 * ONE_SECOND_IN_MILLISEC)
 #define RTSP_KEEP_ALIVE_WAIT_TIMEOUT    ( ONE_SECOND_IN_MILLISEC )
 
 typedef enum rtsp_status_e
@@ -397,7 +398,7 @@ private:
     std::string generate_request_response_msg(RTSP_MSG_FMT_SINK2SRC msg_fmt_needed, std::string received_session_no, std::string append_data1);
     std::string get_RequestSequenceNumber(void);
 
-    RTSP_STATUS receive_buffer_timedOut(int sockfd, void *buffer, size_t buffer_len , unsigned int wait_time_ms = SOCKET_DFLT_WAIT_TIMEOUT );
+    RTSP_STATUS receive_buffer_timedOut(int sockfd, void *buffer, size_t buffer_len , unsigned int wait_time_ms = RTSP_RECV_WAIT_TIMEOUT );
     bool wait_data_timeout(int m_Sockfd, unsigned int ms);
     RTSP_STATUS send_rstp_msg(int sockfd, std::string rtsp_response_buffer);
 };
